6677feba5bf2cec19d38a9abd7ab7d986eeaaf3f
Raul Arrabales <raul.arrabales@gmail.com>
2015-05-18 17:31:09 +0200

    Update HiveScript.sql

--- HiveScript.sql
+++ HiveScript.sql
@@ -1,11 +1,12 @@
 
+
 !connect jdbc:hive2://localhost:10000 scott tiger org.apache.hive.jdbc.HiveDriver 
 
 create database raul_db;
 
 use raul_db;
 
-create table tpa_data (Player INT, PlayerType STRING, PlayerName STRING, JudgeAge INT, JudgeGender STRING, JudgeLevel INT, JudgeEXP STRING, JudgeEDU STRING, JudgeJOB STRING, Guess STRING, Humanness INT, Machinness INT, GuessReason STRING) 
+create table tpa_data (Player INT, PlayerType STRING, PlayerName STRING, JudgeAge INT, JudgeGender STRING, JudgeLevel STRING, JudgeEXP STRING, JudgeEDU STRING, JudgeJOB STRING, Guess STRING, Humanness INT, Machinness INT, GuessReason STRING) 
 row format delimited
 fields terminated by '\;'
 lines terminated by '\n'
@@ -13,4 +14,13 @@ stored as textfile;
 
 LOAD DATA LOCAL INPATH 'Desktop/botprize/TPA_Data_v2.csv' INTO TABLE tpa_data;
 
+show databases;
 
+show tables;
+
+select * from raul_db.tpa_data;
+
+
+select * from raul_db.tpa_data where Player=6;
+
+desc tpa_data;
